53a6b2ff4527fdb55f0e1e8410c9a92e9cd8c553
brandon-nam <brandonnam2020@gmail.com>
2026-01-28 03:36:07 +0800

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: { port: 5173 }
-})
+    plugins: [react()],
+    base: "/portfolio/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    server: { port: 5173 },
+});
