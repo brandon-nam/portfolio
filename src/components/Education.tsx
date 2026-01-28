bb8bb4d7a7adb15786fb1ef85a90f0e5d21fe999
brandon-nam <brandonnam2020@gmail.com>
2026-01-28 23:42:29 +0800

    Edit education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -5,29 +5,29 @@ import Gallery from "./ui/gallery";
 const images = [];
 
 export default function Education() {
-    const [currentIndex, setCurrentIndex] = React.useState(0);
+  const [currentIndex, setCurrentIndex] = React.useState(0);
 
-    return (
-        <section id="education">
-            <h2 className="text-2xl font-semibold">Education</h2>
-            <p className="mt-4 text-slate-600"></p>
-            <div className="mt-4 flex flex-col sm:grid-cols-2 gap-6">
-                <Card className="transition-all duration-300 hover:scale-105 hover:shadow-md">
-                    <CardHeader>
-                        <div className="flex justify-between items-center">
-                            <div className="flex flex-row justify-between gap-2">
-                                <CardTitle>BSc. in Computer Science (Honours)</CardTitle>
-                                <CardTitle>|</CardTitle>
-                                <CardTitle>2020 - 2026</CardTitle>
-                            </div>
-                        </div>
-                        <CardDescription className="mt-2">
-                            Took a leave of absence in 2021 to serve in the Republic of Korea Army.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardFooter />
-                </Card>
+  return (
+    <section id="education">
+      <h2 className="text-2xl font-semibold">Education</h2>
+      <p className="mt-4 text-slate-600"></p>
+      <div className="mt-4 flex flex-col sm:grid-cols-2 gap-6">
+        <Card className="transition-all duration-300 hover:scale-105 hover:shadow-md">
+          <CardHeader>
+            <div className="flex justify-between items-center">
+              <div className="flex flex-row justify-between gap-2">
+                <CardTitle>National University of Singapore - BSc. in Computer Science (Honours)</CardTitle>
+                <CardTitle>|</CardTitle>
+                <CardTitle>2020 - 2026</CardTitle>
+              </div>
             </div>
-        </section>
-    );
+            <CardDescription className="mt-2">
+              Took a leave of absence in 2021 to serve in the Republic of Korea Army.
+            </CardDescription>
+          </CardHeader>
+          <CardFooter />
+        </Card>
+      </div>
+    </section>
+  );
 }
